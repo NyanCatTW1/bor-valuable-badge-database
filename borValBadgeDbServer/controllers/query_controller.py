cd47094791c0bc566dc9af871c3a1d7dc4a20103
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2023-05-07 18:21:25 +0800

    Implement /query/*

--- borValBadgeDbServer/controllers/query_controller.py
+++ borValBadgeDbServer/controllers/query_controller.py
@@ -1,6 +1,10 @@
 from borValBadgeDbServer.models.query_by_badge_ids_get200_response import QueryByBadgeIdsGet200Response  # noqa: E501
 from borValBadgeDbServer.models.query_by_universe_ids_get200_response import QueryByUniverseIdsGet200Response  # noqa: E501
-from borValBadgeDbServer import util
+
+from borValBadgeDbServer.models.badge_info import BadgeInfo
+from borValBadgeDbServer.models.universe_info import UniverseInfo
+
+from borValBadgeDbServer.db.db import dbLock, badgeDB, getBadgeIdCache
 
 
 def query_by_badge_ids_get(badge_ids):  # noqa: E501
@@ -13,7 +17,21 @@ def query_by_badge_ids_get(badge_ids):  # noqa: E501
 
     :rtype: Union[QueryByBadgeIdsGet200Response, Tuple[QueryByBadgeIdsGet200Response, int], Tuple[QueryByBadgeIdsGet200Response, int, Dict[str, str]]
     """
-    return 'do some magic!'
+
+    badge_ids = {str(x) for x in badge_ids}
+    ret = []
+
+    dbLock.acquire()
+    for universeId in badgeDB.universes.keys():
+        idsToGet = badge_ids & getBadgeIdCache(universeId)
+        for badgeId in idsToGet:
+            ret.append(badgeDB.universes[universeId].badges[badgeId])
+        badge_ids -= idsToGet
+    dbLock.release()
+
+    for missingId in badge_ids:
+        ret.append(BadgeInfo(missingId, False))
+    return QueryByBadgeIdsGet200Response(ret)
 
 
 def query_by_badge_ids_post(body):  # noqa: E501
@@ -26,7 +44,8 @@ def query_by_badge_ids_post(body):  # noqa: E501
 
     :rtype: Union[QueryByBadgeIdsGet200Response, Tuple[QueryByBadgeIdsGet200Response, int], Tuple[QueryByBadgeIdsGet200Response, int, Dict[str, str]]
     """
-    return 'do some magic!'
+
+    return query_by_badge_ids_get(body.decode().split(","))
 
 
 def query_by_universe_ids_get(universe_ids):  # noqa: E501
@@ -39,7 +58,19 @@ def query_by_universe_ids_get(universe_ids):  # noqa: E501
 
     :rtype: Union[QueryByUniverseIdsGet200Response, Tuple[QueryByUniverseIdsGet200Response, int], Tuple[QueryByUniverseIdsGet200Response, int, Dict[str, str]]
     """
-    return 'do some magic!'
+
+    universe_ids = {str(x) for x in universe_ids}
+    ret = []
+    dbLock.acquire()
+    idsToGet = universe_ids & set(badgeDB.universes.keys())
+    for universeId in idsToGet:
+        ret.append(badgeDB.universes[universeId])
+    dbLock.release()
+
+    for missingId in universe_ids - idsToGet:
+        ret.append(UniverseInfo(missingId, False))
+
+    return QueryByUniverseIdsGet200Response(ret)
 
 
 def query_by_universe_ids_post(body):  # noqa: E501
@@ -52,4 +83,5 @@ def query_by_universe_ids_post(body):  # noqa: E501
 
     :rtype: Union[QueryByUniverseIdsGet200Response, Tuple[QueryByUniverseIdsGet200Response, int], Tuple[QueryByUniverseIdsGet200Response, int, Dict[str, str]]
     """
-    return 'do some magic!'
+
+    return query_by_universe_ids_get(body.decode().split(","))
