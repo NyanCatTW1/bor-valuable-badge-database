1def64166bc4d47fcba14645bf63a4b650422338
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2023-06-04 14:35:09 +0800

    Listen to 0.0.0.0

--- borValBadgeDbServer/__main__.py
+++ borValBadgeDbServer/__main__.py
@@ -21,7 +21,7 @@ def main():
                 arguments={'title': 'BoR Valuable Badge Database'},
                 pythonic_params=True)
 
-    app.run(port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 
 if __name__ == '__main__':
